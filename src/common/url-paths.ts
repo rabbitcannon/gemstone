06243a3613f73112859354d8af02517d0ec55265
Robert Blahut <anesthetikal@gmail.com>
2021-06-07 15:48:44 -0400

    Fixed url pathing from forgotten static values, because deep down, I'm not a good developer.

--- src/common/url-paths.ts
+++ src/common/url-paths.ts
@@ -1,17 +1,18 @@
-const baseURL = process.env.REACT_APP_BEDROCK_URL
+const BASE_API_URL = process.env.REACT_APP_BEDROCK_API_URL
+const BASE_BEDROCK_URL = process.env.REACT_APP_BEDROCK_API_URL
 
 /* eslint-disable no-unused-vars */
 export const authPaths = {
-  CSRF_COOKIE_URL: 'http://portal.test/api/sanctum/csrf-cookie',
-  LOGIN_URL: baseURL + 'login',
-  LOGOUT_URL: baseURL + 'logout'
+  CSRF_COOKIE_URL: BASE_API_URL + 'sanctum/csrf-cookie',
+  LOGIN_URL: BASE_BEDROCK_URL + 'login',
+  LOGOUT_URL: BASE_BEDROCK_URL + 'logout'
 }
 
 export const userPaths = {
-  USER_URL: baseURL + 'user'
+  USER_URL: BASE_BEDROCK_URL + 'user'
 }
 
 export const companyPaths = {
-  USER_COMPANY: 'http://portal.test/api/quartz/user/company'
+  USER_COMPANY: BASE_BEDROCK_URL + 'user/company'
 }
 /* eslint-enable no-unused-vars */
